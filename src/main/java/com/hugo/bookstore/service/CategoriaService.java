3610695c66c397dc41d0e46ceec523347837cd30
HugoRodrigoo <hugorbarradas@gmail.com>
2022-05-24 19:06:19 -0300

    POST para categoria

--- src/main/java/com/hugo/bookstore/service/CategoriaService.java
+++ src/main/java/com/hugo/bookstore/service/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.hugo.bookstore.domain.Categoria;
@@ -24,5 +25,10 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll(){
 		return repository.findAll();
+	}
+	
+	public Categoria criate(Categoria obj) {
+		obj.setId(null);
+		return repository.save(obj);
 	}
 }
