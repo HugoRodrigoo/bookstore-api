d2be648a83b1a083c4923bfdc5077ec36d775006
HugoRodrigoo <hugorbarradas@gmail.com>
2022-05-26 00:29:44 -0300

    DELETE para Categoria

--- src/main/java/com/hugo/bookstore/service/CategoriaService.java
+++ src/main/java/com/hugo/bookstore/service/CategoriaService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.hugo.bookstore.domain.Categoria;
@@ -38,5 +37,11 @@ public class CategoriaService {
 		obj.setNome(objDto.getNome());
 		obj.setDiscricao(objDto.getDiscricao());
 		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		findById(id);
+		repository.deleteById(id);
+		
 	}
 }
