5ecfd32473c0c84a17aeaa7c92ed88e44c4c2374
HugoRodrigoo <hugorbarradas@gmail.com>
2022-05-28 14:52:34 -0300

    PUT e PATCH para Livro

--- src/main/java/com/hugo/bookstore/service/LivroService.java
+++ src/main/java/com/hugo/bookstore/service/LivroService.java
@@ -19,6 +19,9 @@ public class LivroService {
 	@Autowired
 	private CategoriaService cateService;
 	
+	
+	
+	
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -28,5 +31,18 @@ public class LivroService {
 	public List<Livro> findAll(Integer id_cat) {
 		cateService.findById(id_cat);
 		return repository.findAllByCategoria(id_cat);
+	}
+
+	public Livro update(Integer id, Livro obj) {
+		Livro newObj = findById(id);
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	private void updateData(Livro newObj, Livro obj) {
+		newObj.setTitulo(obj.getTitulo());
+		newObj.setNome_autor(obj.getNome_autor());
+		newObj.setTexto(obj.getTexto());
+		
 	}
 }
